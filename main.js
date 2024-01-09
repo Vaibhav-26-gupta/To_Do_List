e68da20a79aa3c380bfbce08a97a3838c679703a
Vaibhav-26-gupta <guptavaibhav2602@gmail.com>
2024-01-09 23:11:06 +0530

    qwerty

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ window.addEventListener('load', () => {
 	const list_el = document.querySelector("#tasks");
 
 	form.addEventListener('submit', (e) => {
+		e.preventDefault();
 
 		const task = input.value;
 
